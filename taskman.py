550b6a774f3ccbea89ef52d63e7dc825032fc23c
Carl Lemaire <c.lemaire@live.ca>
2018-04-10 16:19:08 -0400

    WIP submit

--- taskman.py
+++ taskman.py
@@ -2,10 +2,45 @@ import subprocess
 import inspect
 import time
 
+from os import makedirs
 
-def submit(template_file, args_str):
-    print(template_file)
-    print(args_str)
+
+def submit(template_file, args_str, task_name):
+    # Generate id
+    task_id = time.strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Get template
+    with open(template_file, 'r') as f:
+        script_lines = f.readlines()
+
+    # Append post exec bash script
+    with open('$HOME/script_moab/taskman_post_exec.sh', 'r') as f:
+        post_exec = f.readlines()
+    script_lines += post_exec
+
+    # Replace variables
+    for line in script_lines:
+        line.replace('$TASKMAN_NAME', task_name)
+        line.replace('$TASKMAN_ID', task_id)
+        line.replace('$TASKMAN_ARGS', args_str)
+
+    # Write script
+    script_path = '$HOME/script_moab/taskman/' + task_name
+    makedirs(script_path)
+    script_file = script_path + '/' + task_id + '.sh'
+    with open(script_file, 'w') as f:
+        f.writelines(script_lines)
+
+    # Submit using msub
+    output = ""
+    try:
+        output = subprocess.check_output(['msub', script_file], stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError:
+        print('ERROR using msub:')
+        print(output)
+
+    # Get moab job id
+    moab_id = output.strip()
 
 
 cmds = {'sub': submit}
@@ -13,7 +48,9 @@ cmds = {'sub': submit}
 
 def handle_command(cmd_str):
     tokens = cmd_str.split(' ')
-    cmds[tokens[0]](*tokens[1:])
+    cmd_name = tokens[0]
+    cmd_args = ' '.join(tokens[1:])
+    cmds[cmd_name](cmd_args.split(','))
 
 
 def show_commands():
