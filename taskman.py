ff1f2ad1e03a1818e3d3887ac27955c7fdf9463f
Carl Lemaire <c.lemaire@live.ca>
2019-02-01 16:03:53 -0500

    Use default maxlines value

--- taskman.py
+++ taskman.py
@@ -14,7 +14,7 @@ DB_STARTED_TASKS = HOMEDIR + '/taskman/started'
 SCRIPTS_FOLDER = env_vars.get('TASKMAN_SCRIPTS', HOMEDIR + '/script_moab')  # Dir with your scripts. Contains /taskman
 CKPT_FOLDER = env_vars['TASKMAN_CKPTS']
 SLURM_MODE = 'TASKMAN_USE_SLURM' in env_vars
-MAX_LINES = env_vars['TASKMAN_MAXLINES']
+MAX_LINES = env_vars.get('TASKMAN_MAXLINES', 30)
 
 
 def fmt_time(seconds):
